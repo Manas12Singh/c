fd9a8f7214bf23cba1b11003ba0f595b46102413
Manas12Singh <rd511323@gmail.com>
2023-01-22 19:24:55 +0530

    just modified

--- cramers_rule.c
+++ cramers_rule.c
@@ -1,19 +1,43 @@
 #include<stdio.h>
 
+int r;
+void cramer_rule(int (*)[r],int,int);
+
 int main()
 {
-    int r,x,y,z,t;
     printf("Enter the number of variables: ");
     scanf("%d",&r);
-    float m[r][r];
+    float a[r][r],b[r];
     printf("Enter the elements of matrix A:\n");
     for (int i = 0; i < r; i++)
     {
         for (int j = 0; i < r; i++)
         {
-            scanf("%f",&m[i][j]);
+            scanf("%f",&a[i][j]);
         }
     }
-    
+    printf("Enter the elements of matrix B:\n");
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < r; j++)
+        {
+            scanf("%f",&b[i]);
+        }
+    }
+    cramer_rule(a[r],r,b);
     return 0;
+}
+
+void cramer_rule(int (*a)[r],int r,int b)
+{
+    int x,y,z,t,swap;
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < count; j++)
+        {
+            /* code */
+        }
+        
+    }
+    
 }
