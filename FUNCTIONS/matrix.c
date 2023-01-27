160b4dd9d2dcc1469038a675763afa3f79a05aec
Manas12Singh <rd511323@gmail.com>
2023-01-27 11:42:18 +0530

    m

--- FUNCTIONS/matrix.c
+++ FUNCTIONS/matrix.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
 
 int c;
-void rank(int,int, int (*)[]);
-void j;
+void rank(int,int, int (*)[c]);
 
 int main()
 {
-    int r,c;
+    int r,c,i,j;
     printf("No. of row(s) of matrix: ");
     scanf("%d", &r); // takes the no. of rows of matrix
     printf("No. of column(s) of matrix: ");
     scanf("%d", &c); // takes the no. of rows of matrix
 
     int matrix[r][c];
-    for (int i = 0; i < r; i++) // takes row wise elements of matrix like: 12 23 -23
-    {
-        printf("Enter elements of row %d:\n", i + 1);
-        for (int j = 0; j < c; j++)
+    printf("Enter elements of the matrix:\n");
+    for (i = 0; i < r; i++) // takes row wise elements of matrix like: 12 23 -23
+        for (j = 0; j < c; j++)
             scanf("%d", &matrix[i][j]);
-    }
 
     return 0;
 }
 
 void rank(int r,int c,int (*m)[c])
 {
-
+    printf("Mnaas");
 }
