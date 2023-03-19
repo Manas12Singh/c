bcea0086416329db8697f02c395821ef1770f730
Manas12Singh <rd511323@gmail.com>
2023-03-19 07:36:11 +0530

    ok

--- checks.c
+++ checks.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
-#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-int main(){
-    for(int i=0;i<200;i++)
-        if(ispunct(i))
-            printf("%c",i);
-    return 0;
+bool s(char *sen, char *search)
+{
+    int k = 1, i = 0;
+    while (*sen != '\0')
+    {
+        if (k == 1 || *(sen - 1) == ' ')
+        {
+            if (*sen == search[i])
+            {
+                i++;
+                if (i == strlen(search) && (*(sen + 1) == ' ' || *(sen + 1) == '\0'))
+                    return true;
+            }
+            else
+                i = 0;
+        }
+        k = 0;
+        sen++;
+    }
+    return false;
 }
-
+void main()
+{
+    char sen[100], search[20];
+    gets(sen);
+    gets(search);
+    if (s(sen, search))
+        printf("Found");
+    else
+        printf("Not found");
+}
