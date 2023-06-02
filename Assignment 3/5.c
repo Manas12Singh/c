d421c7009441e653c6389e48a45785f44a36c3df
Manas12Singh <rd511323@gmail.com>
2023-06-02 20:24:06 +0530

    done

--- Assignment 3/5.c
+++ Assignment 3/5.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 void main()
 {
@@ -21,3 +22,30 @@ void main()
     else
         printf("Not Found");
 }
+
+/*
+Alternate:
+#include <stdio.h>
+#include <string.h>
+
+void main()
+{
+    char ecode[5], data[100];
+    char *sal = NULL;
+    FILE *fp = fopen("Database.txt", "r");
+    scanf("%s", ecode);
+    while (!feof(fp))
+    {
+        fgets(data, 100, fp);
+        if (strncmp(ecode, data, 4) == 0)
+        {
+            sal = strrchr(data, ' ') + 1;
+            break;
+        }
+    }
+    if (sal != NULL)
+        printf("Salary: %s", sal);
+    else
+        printf("Not Found");
+}
+*/
