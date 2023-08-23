c2cf5b897adcb97be3228df98c8193941f3d3c5a
Manas12Singh <rd511323@gmail.com>
2023-08-23 19:07:25 +0530

    run
    s

--- run.c
+++ run.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <conio.h>
 int main()
 {
+    char ch[100];
+    system("cls");
+    printf("I1\\Manas Singh\\38>");
+    gets(ch);
+    system(ch);
+    system("cls");
+    printf("I1\\Manas Singh\\38>%s\n", ch);
+    printf("I1\\Manas Singh\\38>a\n");
+    system("a");
     return 0;
 }
