a3f487eb51ca594578ffd478432fcbf7f641b15f
Manas12Singh <rd511323@gmail.com>
2024-10-07 14:17:14 +0530

    Refactor code for round-robin scheduling algorithm

--- 3rdYear/OSLab/w4q1p1.c
+++ 3rdYear/OSLab/w4q1p1.c
@@ -55,46 +55,6 @@ void priority(int n, Process *processes, double *avgWaitTime, double *avgTurnAro
     *avgWaitTime /= n;
     *avgTurnAroundTime /= n;
 }
-void roundRobin(int n, Process *processes, double *avgWaitTime, double *avgTurnAroundTime, int timeQuantum){
-    for(int i = 0; i < n; i++)
-        processes[i].remainingTime = processes[i].burstTime;
-    qsort(processes, n, sizeof(Process), compareArrival);
-    *avgWaitTime = 0;
-    *avgTurnAroundTime = 0;
-    int currentTime = processes[0].arrivalTime;
-    int i = 0;
-    int prev = -1;
-    printf("Round Robin Gantt Chart: ");
-    while (i < n)
-    {
-        for (int j = i; j < n && processes[j].arrivalTime <= currentTime; j++)
-        {
-            if (prev != processes[j].id)
-            {
-                printf("P%d ", processes[j].id);
-                prev = processes[j].id;
-            }
-            int runTime = min(processes[j].remainingTime, timeQuantum);
-            processes[j].remainingTime -= runTime;
-            currentTime += runTime;
-            if (processes[j].remainingTime == 0)
-            {
-                *avgWaitTime += currentTime - processes[j].arrivalTime - processes[j].burstTime;
-                *avgTurnAroundTime += currentTime - processes[j].arrivalTime;
-                if (j != i)
-                {
-                    Process temp = processes[j];
-                    processes[j] = processes[i];
-                    processes[i] = temp;
-                }
-                i++;
-            }
-        }
-    }
-    printf("\n");
-    *avgWaitTime /= n;
-    *avgTurnAroundTime /= n;
-}
 int main(){
     int n, timeQuantum;
     printf("Enter number of processes: ");
@@ -116,9 +76,6 @@ int main(){
     scanf("%d", &timeQuantum);
     double avgWaitTime, avgTurnAroundTime;
     priority(n, processes, &avgWaitTime, &avgTurnAroundTime, timeQuantum);
-    printf("Average Waiting Time: %.2lf\n", avgWaitTime);
-    printf("Average Turnaround Time: %.2lf\n", avgTurnAroundTime);
-    roundRobin(n, processes, &avgWaitTime, &avgTurnAroundTime, timeQuantum);
     printf("Average Waiting Time: %.2lf\n", avgWaitTime);
     printf("Average Turnaround Time: %.2lf\n", avgTurnAroundTime);
     free(processes);
