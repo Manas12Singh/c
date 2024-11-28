01a3b2d0e97d25a396bdfb961cfbdf4c8b68c40e
Manas12Singh <rd511323@gmail.com>
2024-11-28 17:09:58 +0530

    Add least squares method for linear regression and user input handling

--- 3rdYear/CBNSTLab/14.c
+++ 3rdYear/CBNSTLab/14.c
@@ -1,3 +1,34 @@
 #include <stdio.h>
 
-#define f(x, a, b) (a + b * x)
+void leastSquareMethod(int n, double x[], double y[])
+{
+    double sumX = 0, sumY = 0, sumXY = 0, sumX2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sumX += x[i];
+        sumY += y[i];
+        sumXY += x[i] * y[i];
+        sumX2 += x[i] * x[i];
+    }
+    double b = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX * sumX);
+    double a = (sumY - b * sumX) / n;
+    printf("The line is: y = %.4fx + %.4f\n", b, a);
+}
+
+int main()
+{
+    int n;
+    printf("Enter the number of data points: ");
+    scanf("%d", &n);
+    double x[n], y[n];
+    printf("Enter the data points:\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter x%d: ", i);
+        scanf("%lf", &x[i]);
+        printf("Enter y%d: ", i);
+        scanf("%lf", &y[i]);
+    }
+    leastSquareMethod(n, x, y);
+    return 0;
+}
